daa52b1b7633ced89a4b0a1dbf06228f88946c82
sunpawets <sunpawet.somsin@thomsonreuters.com>
2015-10-05 18:49:10 +0700

    add files

---
+++ BattleshipBoard.cs
@@ -0,0 +1,124 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CodeChallenge1
+{
+    public enum Result { HIT, MISS, MISSION_COMPLETED };
+
+
+    interface IFireable
+    {
+        Result Fire(int column, int row);
+    }
+    public class BattheshipBoard : IFireable
+    {
+        private int MAX_TOTAL_HIT = 17;
+        private int START_INDEX = 1;
+        private int END_INDEX = 10;
+        private int hitCount = 0;
+        private int hitShipCount = 0;
+        private string[,] boardOriginal;
+        private string[,] board = new string[10, 10] 
+        {
+            {".","X",".",".",".",".",".",".",".","."},
+            {".","X",".",".",".",".","X","X","X","."},
+            {".","X",".",".",".",".",".",".","X","."},
+            {".","X",".",".",".",".",".",".","X","."},
+            {".","X",".","X","X",".",".",".","X","."},
+            {".",".",".",".",".",".",".","X",".","."},
+            {".",".",".",".",".",".",".","X",".","."},
+            {".",".",".",".",".",".",".","X",".","."},
+            {".",".",".",".",".",".",".","X",".","."},
+            {".",".",".",".",".",".",".",".",".","."}
+        };
+
+        public BattheshipBoard()
+        {
+            hitCount = 0;
+            hitShipCount = 0;
+            boardOriginal = (string[,]) board.Clone();
+        }
+
+        public Result Fire(int column, int row) 
+        {
+            if (column < START_INDEX || column > END_INDEX || row < START_INDEX || row > END_INDEX)
+                throw new ArgumentException("A column and row number must be a value between 1 to 10.");
+
+            column--;
+            row--;
+            hitCount++;
+
+            if (IsMissionCompleted())
+                return Result.MISSION_COMPLETED;
+
+            if (boardOriginal[row, column] == "X")
+            {                
+                board[row, column] = ".";
+                return IsMissionCompleted() ? Result.MISSION_COMPLETED : Result.HIT;
+            }
+            
+            return Result.MISS;
+        }
+
+        private bool IsMissionCompleted()
+        {
+            foreach (var item in board)
+            {
+                if (item == "X")
+                {
+                    return false;
+                }
+            }
+            return true;
+        }
+
+        public object GetHit()
+        {
+            return hitCount;
+        }
+    }
+    //public class BattleshipBoardX 
+    //{
+    //    public override void Play(IFireable fireable)
+    //    {
+    //        // Implement AI logic here
+
+    //        #region example code 
+            
+    //        var isCompleted = false;
+    //        for (int i = 1; i <= 10; i++)
+    //        {
+    //            for (int j = 1; j <= 10; j++)
+    //            {
+    //                var result = fireable.Fire(i, j);
+    //                PrintBoard();
+    //                if (result == Result.MISSION_COMPLETED)
+    //                {
+    //                    isCompleted = true;
+    //                    break;
+    //                }
+    //            }
+    //            if (isCompleted)
+    //                break;
+    //        }
+            
+    //        #endregion
+    //    }
+
+    //    public void PrintBoard()
+    //    {
+    //        Console.Clear();
+    //        for (int i = 0; i < 10; i++)
+    //        {
+    //            for (int j = 0; j < 10; j++)
+    //            {
+    //                Console.Write(board[i, j] ?? ".");
+    //            }
+    //            Console.WriteLine();
+    //        }
+    //    }
+    //}
+}
---
+++ CodeChallengeTest/FireTest.cs
@@ -0,0 +1,117 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using CodeChallenge1;
+
+namespace CodeChallengeTest
+{
+    [TestClass]
+    public class FireTest
+    {
+        [TestMethod]
+        [ExpectedException(typeof(ArgumentException), "A column and row number must be a value between 1 to 10.")]
+        public void Fire_ColumnOverThan10ShouldThrowException()
+        {
+            BattheshipBoard board = new BattheshipBoard();
+            board.Fire(11, 1);
+        }
+
+        [TestMethod]
+        [ExpectedException(typeof(ArgumentException), "A column and row number must be a value between 1 to 10.")]
+        public void Fire_ColumnLowerThan0ShouldThrowException()
+        {
+            BattheshipBoard board = new BattheshipBoard();
+            board.Fire(0, 1);
+        }
+
+        [TestMethod]
+        [ExpectedException(typeof(ArgumentException), "A column and row number must be a value between 1 to 10.")]
+        public void Fire_RowOverThan10ShouldThrowException()
+        {
+            BattheshipBoard board = new BattheshipBoard();
+            board.Fire(1, 11);
+        }
+
+        [TestMethod]
+        [ExpectedException(typeof(ArgumentException), "A column and row number must be a value between 1 to 10.")]
+        public void Fire_RowLowerThan0ShouldThrowException()
+        {
+            BattheshipBoard board = new BattheshipBoard();
+            board.Fire(1, 0);
+        }
+
+        [TestMethod]
+        public void Fire_WhenCorrectColumnAndRow_ThenReturnHIT()
+        {
+            BattheshipBoard board = new BattheshipBoard();
+            var result = board.Fire(2, 1);
+
+            Assert.AreEqual(Result.HIT, result);
+        }
+
+        [TestMethod]
+        public void Fire_WhenIncorrectColumnAndRow_ThenReturnMISS()
+        {
+            BattheshipBoard board = new BattheshipBoard();
+            var result = board.Fire(1, 1);
+
+            Assert.AreEqual(Result.MISS, result);
+        }
+
+        [TestMethod]
+        public void Fire_WhenCorrectAll_ThenReturnCompleted()
+        {
+            BattheshipBoard board = new BattheshipBoard();
+            // Ship 5 length
+            var result = board.Fire(2, 1);
+            Assert.AreEqual(Result.HIT, result);
+            result = board.Fire(2, 2);
+            Assert.AreEqual(Result.HIT, result);
+            result = board.Fire(2, 3);
+            Assert.AreEqual(Result.HIT, result);
+            result = board.Fire(2, 4);
+            Assert.AreEqual(Result.HIT, result);
+            result = board.Fire(2, 5);
+            Assert.AreEqual(Result.HIT, result);
+
+            // Ship 2 length
+            result = board.Fire(4, 5);
+            Assert.AreEqual(Result.HIT, result);
+            result = board.Fire(5, 5);
+            Assert.AreEqual(Result.HIT, result);
+
+            // Ship 3 length x 2
+            result = board.Fire(7, 2);
+            Assert.AreEqual(Result.HIT, result);
+            result = board.Fire(8, 2);
+            Assert.AreEqual(Result.HIT, result);
+            result = board.Fire(9, 2);
+            Assert.AreEqual(Result.HIT, result);
+            result = board.Fire(9, 3);
+            Assert.AreEqual(Result.HIT, result);
+            result = board.Fire(9, 4);
+            Assert.AreEqual(Result.HIT, result);
+            result = board.Fire(9, 5);
+            Assert.AreEqual(Result.HIT, result);
+
+            // Ship 4 length 
+            result = board.Fire(8, 6);
+            Assert.AreEqual(Result.HIT, result);
+            result = board.Fire(8, 7);
+            Assert.AreEqual(Result.HIT, result);
+            result = board.Fire(8, 7);
+            Assert.AreEqual(Result.HIT, result);
+            result = board.Fire(1, 1);
+            Assert.AreEqual(Result.MISS, result);
+            result = board.Fire(8, 8);
+            Assert.AreEqual(Result.HIT, result);
+            result = board.Fire(8, 9);
+            Assert.AreEqual(Result.MISSION_COMPLETED, result);
+            result = board.Fire(8, 9);
+            Assert.AreEqual(Result.MISSION_COMPLETED, result);
+            result = board.Fire(1, 1);
+            Assert.AreEqual(Result.MISSION_COMPLETED, result);
+
+            Assert.AreEqual(21, board.GetHit());
+        }
+    }
+}
